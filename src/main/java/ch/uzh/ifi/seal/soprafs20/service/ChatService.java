b7c81ceab8bbaf2885b4b9dbab764c4b2ce2aaec
Alestry <45560364+Alestry@users.noreply.github.com>
2020-03-28 14:16:13 +0100

    Added new things 4

--- src/main/java/ch/uzh/ifi/seal/soprafs20/service/ChatService.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/service/ChatService.java
@@ -1,16 +1,21 @@
 package ch.uzh.ifi.seal.soprafs20.service;
 
 import ch.uzh.ifi.seal.soprafs20.chat.Chat;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Takes care of actions regarding the chat.
  */
+@Service
+@Transactional
 public class ChatService {
 
-    public List<String> getHistory(Chat chat){
-        return chat.getHistory();
+
+    public List<String> getHistory(String chatmode, long gameId){
+        return null;
     }
 
     public void newMessage(Chat chat, String message){
