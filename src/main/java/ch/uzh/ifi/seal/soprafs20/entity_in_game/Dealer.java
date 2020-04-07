a6618661c1b51e659eb78a6d6d4711047b9e1312
Alestry <45560364+Alestry@users.noreply.github.com>
2020-04-07 13:57:09 +0200

    Added some details to Player class

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/Dealer.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/Dealer.java
@@ -1,4 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.entity_in_game;
 
 public class Dealer extends Player {
+    Dealer(int credit, long id) {
+        super(credit, id);
+    }
 }
--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/Player.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/Player.java
@@ -1,4 +1,45 @@
 package ch.uzh.ifi.seal.soprafs20.entity_in_game;
 
+import ch.uzh.ifi.seal.soprafs20.cards.Card;
+
+import java.util.List;
+
 public class Player{
+
+    private List<Card> hand;
+
+    private int credit;
+
+    private long id;
+
+    Player(int credit, long id){
+        this.credit = credit;
+        this.id = id;
+    }
+
+
+    public void addCredit(int amount){
+        credit += amount;
+    }
+
+    public void removeCredit(int amount){
+        credit -= amount;
+    }
+
+    public int getCredit(){
+        return credit;
+    }
+
+    public void addToHand(Card card){
+        hand.add(card);
+    }
+
+    public List<Card> getHand(){
+        return hand;
+    }
+
+    public long getId(){
+        return id;
+    }
+
 }
