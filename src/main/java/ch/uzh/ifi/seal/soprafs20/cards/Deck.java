b085bf40e707f1dde3f2c2bdb1d9881341b670bd
kevinkindler <55450495+kevinkindler@users.noreply.github.com>
2020-04-09 17:32:27 +0200

    Class Card and Deck implemented

--- src/main/java/ch/uzh/ifi/seal/soprafs20/cards/Deck.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/cards/Deck.java
@@ -1,7 +1,37 @@
 package ch.uzh.ifi.seal.soprafs20.cards;
 
+import ch.uzh.ifi.seal.soprafs20.constant.Rank;
+import ch.uzh.ifi.seal.soprafs20.constant.Suit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Deck {
 
-    public Card getTopCard(){return null;}
+    private java.util.List<Card> cards = new ArrayList<>();
+
+    public Deck(){
+        for (Suit s: Suit.values()){
+            for (Rank r: Rank.values()){
+                Card c = new Card(s, r);
+                cards.add(c);
+            }
+        }
+    }
+
+        public java.util.List<Card> getCards(){
+            return new ArrayList<>(cards);
+        }
+
+        public void shuffle(){
+            Collections.shuffle(this.cards);
+        }
+
+        public Card getTopCard(){
+            Random rand = new Random();
+            Card topCard = this.cards.get(rand.nextInt(this.cards.size()));
+            return new Card(topCard.getSuit(), topCard.getRank());
+        }
 
 }
