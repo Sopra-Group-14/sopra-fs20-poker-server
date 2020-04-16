4290434a5bb954d4602408091224d02a03fcf9e8
kevinkindler <55450495+kevinkindler@users.noreply.github.com>
2020-04-16 14:09:39 +0200

    GameLog updated

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/GameLog.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/GameLog.java
@@ -1,7 +1,10 @@
 package ch.uzh.ifi.seal.soprafs20.entity_in_game;
 
+import ch.uzh.ifi.seal.soprafs20.cards.Card;
 import ch.uzh.ifi.seal.soprafs20.constant.Action;
 import ch.uzh.ifi.seal.soprafs20.constant.GameRound;
+
+import java.util.List;
 
 /**
  * This object is responsible for documenting what happens in a game.
@@ -11,15 +14,23 @@ public class GameLog {
     private int transactionNr, raiseAmount, playerPot, amountToCall, potAmount;
     private GameRound gameRound;
     private String playerName, nextPlayerName;
+    private List<Player> players;
+    private List<Player> activePlayers;
+    private List<Card> revealedCards;
+    private String gameName;
     private long playerId, nextPlayerId;
     private boolean roundOver, gameOver;
     private Action action;
 
     //All the parameters are set in the constructor
-    public GameLog(int transactionNr, GameRound gameRound, Action action, int raiseAmount, String playerName, long playerId, String nextPlayerName, long nextPlayerId, int playerPot, int potAmount, boolean roundOver, boolean gameOver, int amountToCall){
+    public GameLog(int transactionNr, GameRound gameRound, Action action, List<Player> players, List<Player> activePlayers, List<Card> revealedCards, String gameName, int raiseAmount, String playerName, long playerId, String nextPlayerName, long nextPlayerId, int playerPot, int potAmount, boolean roundOver, boolean gameOver, int amountToCall){
         this.transactionNr = transactionNr;
         this.gameRound = gameRound;
         this.action = action;
+        this.players = players;
+        this.activePlayers = activePlayers;
+        this.revealedCards = revealedCards;
+        this.gameName = gameName;
         this.raiseAmount = raiseAmount;
         this.playerName = playerName;
         this.playerId = playerId;
