63a758cb10d6f1d3a4999e7148d32e557119bd64
Alestry <45560364+Alestry@users.noreply.github.com>
2020-05-20 14:18:55 +0200

    added playGameEndsGameIfOnlyOnePlayer() and playGameEndsGameIfOnlyOnePlayerHasCredit()

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/GameServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/GameServiceTest.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.soprafs20.service;
 
 
+import ch.uzh.ifi.seal.soprafs20.constant.Action;
 import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs20.entity.Game;
 import ch.uzh.ifi.seal.soprafs20.entity.GameSelect;
@@ -36,10 +37,13 @@ public class GameServiceTest {
     @Autowired
     private UserService userService;
 
-    /*@BeforeEach
+    @BeforeEach
     public void setup() {
         userRepository.deleteAll();
-    }*/
+        while(gameService.getAllGames().size() > 0){
+            gameService.getAllGames().remove(0);
+        }
+    }
 
     @Test
     public void addJoiningPlayerAddsCorrectPlayer() {
@@ -62,6 +66,54 @@ public class GameServiceTest {
         assertEquals(testPlayer1.getPlayerName(), returnedPlayer1.getPlayerName());
         assertEquals(testPlayer2.getPlayerName(), returnedPlayer2.getPlayerName());
         assertEquals(testPlayer3.getPlayerName(), returnedPlayer3.getPlayerName());
+
+    }
+
+    @Test
+    public void playGameEndsGameIfOnlyOnePlayer(){
+
+        User toCreateUser = new User();
+        toCreateUser.setName("Name");
+        toCreateUser.setPassword("Password");
+        toCreateUser.setUsername("Username");
+
+        User hostUser = userService.createUser(toCreateUser);
+
+        Game game = gameService.createGame("Game", hostUser.getId(), "Fixed");
+
+        game.playGame(Action.BET, hostUser.getId());
+
+    }
+
+    @Test
+    public void playGameEndsGameIfOnlyOnePlayerHasCredit(){
+
+        User toCreateUser = new User();
+        toCreateUser.setName("Name");
+        toCreateUser.setPassword("Password");
+        toCreateUser.setUsername("Username");
+
+        User toCreateUser2 = new User();
+        toCreateUser2.setName("Name2");
+        toCreateUser2.setPassword("Password2");
+        toCreateUser2.setUsername("Username2");
+
+        User hostUser = userService.createUser(toCreateUser);
+        User joiningUser = userService.createUser(toCreateUser2);
+
+        Game game = gameService.createGame("Game", hostUser.getId(), "None");
+
+        gameService.addHost(hostUser.getId(), game);
+        gameService.addJoiningPlayer(joiningUser.getId(), game.getGameId());
+
+        Long hostId = hostUser.getId();
+        Long joinId = joiningUser.getId();
+        Long gameId = game.getGameId();
+
+        int credit = game.getPlayerById(hostId).getCredit();
+        game.getPlayerById(hostId).removeCredit(credit);
+
+        game.playGame(Action.CALL, joinId);
 
     }
 
