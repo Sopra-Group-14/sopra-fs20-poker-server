359b5acd2fc05858daa27d18d5d1428d90fd4421
uzhAndy <andy.aidoo@uzh.ch>
2020-05-24 14:31:53 +0200

    Delete GameSummary.java
    empty class

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/GameSummary.java
+++
@@ -1,7 +0,0 @@
-package ch.uzh.ifi.seal.soprafs20.entity_in_game;
-
-/**
- * This object is returned once a game is over; it has all the relevant information about the end of the game.
- */
-public class GameSummary {
-}
