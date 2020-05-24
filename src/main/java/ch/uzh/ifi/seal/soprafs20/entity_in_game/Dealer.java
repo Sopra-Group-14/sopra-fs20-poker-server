2896a47c93e49652536b8efd1acccf621707eadd
uzhAndy <andy.aidoo@uzh.ch>
2020-05-24 13:05:04 +0200

    removed empty classes

--- src/main/java/ch/uzh/ifi/seal/soprafs20/entity_in_game/Dealer.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.seal.soprafs20.entity_in_game;
-import ch.uzh.ifi.seal.soprafs20.entity.User;
-
-public class Dealer extends Player {
-    //TODO deleted credit from constructor handle credit with update credit
-    Dealer(int credit, User user) {
-        super(user);
-    }
-}
